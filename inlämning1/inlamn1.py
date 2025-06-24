d1af55eb5914354f1ebea705987a2b0f392bc739
Noah <noah.tingbratt@gmail.com>
2025-06-24 13:39:54 +0200

    Update inlamn1.py

--- inlämning1/inlamn1.py
+++ inlämning1/inlamn1.py
@@ -1,5 +1,6 @@
 #TODO Uppgift 1,2,3,4,5,6,7 done on 78 minutes
 
+#%% mycode
 class myfunc:#kod från min leetcode eller github eller någonstans där jag är uppehovsrättsinnehavare för koden
     def inplacefunsort(self,nums):#från leetcode "sort an array" (fun version) gissa vilken ovanlig sorteringsmetod jag använder tips worst case: nlog(n) och in-place
         for index in range(len(nums)):    
@@ -37,7 +38,7 @@ class myfunc:#kod från min leetcode eller github eller någonstans där jag är
         return nums
     
 
-
+#%% uppgift 1
 def uppgift1():
     def uppgift1a():#okej
         s = 0                  # Sätt summa till noll
@@ -52,21 +53,27 @@ def uppgift1():
     uppgift1a()
     uppgift1b()
 
+#%% uppgift 2
 def uppgift2():
-    def uppgift2a():return 9**(1/2); #ja eftersom 9**1/2 tar ju 9**1 först ty proriteringsregler
-    def uppgift2b():return 1.52*10**-8*6.18*10**9*10//1/10;#behövs ej kommenteras 
-    uppgift2a()
-    uppgift2b()
+    def uppgift2a():return 9**(1/2); #ja eftersom 9**1/2 tar ju 9**1 först ty proriteringsregler och sedan dividerar med 2
+    #det står ej skriv ut 9**1/2 men det är enligt följande kod
+    #print(9**1/2)
+    def uppgift2b():return 1.52*10**-8*6.18*10**9;#behövs ej kommenteras 
+    print(uppgift2a())
+    print(f"{uppgift2b():.1f}")
 
+
+#%% uppgift 3
 def uppgift3():
-    def uppgift3a(k,m):
-        f = lambda x : k*x + m #definerade funktionen
+    def uppgift3a():
+        f = lambda x,k,m : k*x + m #definerade funktionen
         return f
     def uppgift3b():
-        v=uppgift3a(2,3)(1)#ta värdena bara
+        v=uppgift3a()(1,2,3)#ta värdena bara
         print(v)
     uppgift3b()
 
+#%% uppgift 4
 def uppgift4():
     def uppgift4a(x):return len(x);#längden på listan
     def uppgift4b(x): print(x[0],x[1],x[-1]);#first, second last
@@ -75,10 +82,11 @@ def uppgift4():
         noahtingb.inplacefunsort(x)
         print(x)
     u=[10,20,3,4,-52]
-    n=uppgift4a(u)
+    print(uppgift4a(u))
     uppgift4b(u)
     uppgift4c(u)
 
+#%% uppgift 5
 def uppgift5():
     def uppgift5a(): #behövs ej kommenteras
         print("svar för uppgift 5a är 15820.0")
@@ -103,20 +111,25 @@ def uppgift5():
                 uppgift5b()     
         netto= var.get("fast avgift")+var.get("antal enheter")*var.get("pris per enhet")
         brutto=netto*1.25
-        print(f"kostnaden utan moms är {netto:.0f} kr \\ - || -  med moms är {brutto:.0f} kr")
+        print(f"kostnaden utan moms är {netto:.0f} kr \n - || -    med moms är {brutto:.0f} kr")
     uppgift5a()
     uppgift5b()
 
+#%% uppgift 6
 def uppgift6():
     import numpy as np
     x,y=np.array([1,2,3]),np.array([4,5,6]) #a
+    print(f"x: {x}\ny: {y}")
     s=x+5 #b
     t=x*8 #c
+    print(f"s: {s}\nt: {t}")
     u=x+y#d
     print(u)#d
     v=np.sqrt(x)#e
     print(v)#e
     del np
+
+#%% uppgift 7
 def uppgift7():
     def uppgift7a():
         import numpy as np
@@ -133,24 +146,26 @@ def uppgift7():
         T = np.loadtxt('T10365.txt')
         #print(T.shape)
         print(f"Maximal temperatur under 10 år kollon {np.argmax(T)%T.shape[1]} och rad {np.argmax(T)//T.shape[1]}")#behövs ej förklaras mer än T.shape för att få kollon and row
-        print(f"Mean feb {np.mean(T[:,31:59]):.2f}")##behövs ej förklaras index 60 för +1
+        print(f"Mean feb {np.mean(T[:,32:59]):.2f}")##behövs ej förklaras index 60 för +1
         print(f"Mean 1985 {np.mean(T[4,:]):.2f}")##behövs ej förklaras indexet 4 motsvarar 1985
     uppgift7b()
+
+#%% inlämning1
 def inlämning1():
-    print("\n\n")
-    #uppgift1()
-    #uppgift2()
-    #uppgift3()
-    #uppgift4()
-    #uppgift5()
-    #uppgift6()
+    print("\n")
+    print("\nUppgift 1:")
+    uppgift1()
+    print("\nUppgift 2:")
+    uppgift2()
+    print("\nUppgift 3:")
+    uppgift3()
+    print("\nUppgift 4:")
+    uppgift4()
+    print("\nUppgift 5:")
+    uppgift5()
+    print("\nUppgift 6:")
+    uppgift6()
+    print("\nUppgift 7:")
     uppgift7()
 
 inlämning1()
-"""
-(c) Använd ett skärminspelningsprogram och spela in en kort video, där du förklarar, rad
-för rad, vad de olika kommandona gör i programmet i uppgift (b) ovan. 
-Visa också hur du
-kör programmet. Videon laddas upp på YouTube och du klipper bara in länken i överst i
-din py-fil. Tips: om du skriver kommentarer till varje rad så är det lättare att förklara när
-du spelar in videon."""
